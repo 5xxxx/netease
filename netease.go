217e552fd492d3649494cc5b42fae7cceaf09eb1
lintao <mrqter@gmail.com>
2020-06-11 08:48:54 +0800

    增加部分文档

--- netease.go
+++ netease.go
@@ -1,12 +1,31 @@
 /*
- *
- * nimskd.go
- * netease-im
- *
- * Created by lintao on 2020/6/9 9:51 上午
- * Copyright © 2020-2020 LINTAO. All rights reserved.
- *
- */
+网易云信IM服务端SDK
+
+Example:
+
+package main
+
+import (
+	"fmt"
+	"github.com/NSObjects/netease"
+)
+
+func main() {
+	n := netease.NewNetEaseIM("appkey", "secret")
+	token, err := n.CreateAccount(netease.Account{
+		Accid: "xx",
+		Name:  "xx",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(token)
+}
+
+
+Learn more at https://dev.yunxin.163.com/docs/product/IM%E5%8D%B3%E6%97%B6%E9%80%9A%E8%AE%AF/%E6%9C%8D%E5%8A%A1%E7%AB%AFAPI%E6%96%87%E6%A1%A3/%E6%8E%A5%E5%8F%A3%E6%A6%82%E8%BF%B0
+*/
 
 package netease
 
