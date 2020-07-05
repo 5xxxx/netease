7fd1fdd5fb7e9984464116fdd8f8fd1f298fd2f4
lintao <mrqter@gmail.com>
2020-07-05 17:30:59 +0800

    调整测试文件

--- account_test.go
+++ account_test.go
@@ -44,26 +44,68 @@ func TestNetEaseIM_CreateAccount(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name:    "创建账号",
-			fields:  fields{
-				appKey:   AppKey,
-				secret:   Secret,
+			name: "创建账号",
+			fields: fields{
+				appKey: AppKey,
+				secret: Secret,
 			},
-			args:    args{Account{
-				Accid:  "9527111",
+			args: args{Account{
+				Accid: "5e82ddaf7da7a00001c12ef8",
 			}},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			n := NewNetEaseIM(tt.fields.appKey,tt.fields.secret)
+			n := NewNetEaseIM(tt.fields.appKey, tt.fields.secret)
 			got, err := n.CreateAccount(tt.args.account)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("CreateAccount() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			fmt.Println(got)
+		})
+	}
+}
+
+func TestNetEaseIM_RefreshToken(t *testing.T) {
+	type fields struct {
+		appKey   string
+		secret   string
+		basePath string
+	}
+	type args struct {
+		accid string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "刷新Id",
+			fields: fields{
+				appKey: AppKey,
+				secret: Secret,
+			},
+			args:    args{accid: "15274894959"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := NewNetEaseIM(tt.fields.appKey, tt.fields.secret)
+			got, err := n.RefreshToken(tt.args.accid)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RefreshToken() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			fmt.Println(got)
+			//if got != tt.want {
+			//	t.Errorf("RefreshToken() got = %v, want %v", got, tt.want)
+			//}
 		})
 	}
 }
--- msg_test.go
+++ msg_test.go
@@ -10,7 +10,10 @@
 
 package netease
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestNetEaseIM_SendBatchAttachMsg(t *testing.T) {
 	type fields struct {
@@ -31,7 +34,7 @@ func TestNetEaseIM_SendBatchAttachMsg(t *testing.T) {
 			name: "测试批量发送消息",
 			args: args{AttachMsg{
 				FromAccid: "13802426870",
-				ToAccids:  []string{"9527111"},
+				ToAccids:  []string{"15675132016"},
 				Attach:    "{`heihei`}",
 				Save:      2,
 			}},
@@ -43,6 +46,67 @@ func TestNetEaseIM_SendBatchAttachMsg(t *testing.T) {
 			n := NewNetEaseIM("1823d2e114e071e7670cdd520e76cecf", "8bdf74978155")
 			if err := n.SendBatchAttachMsg(tt.args.msg); (err != nil) != tt.wantErr {
 				t.Errorf("SendBatchAttachMsg() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestNetEaseIM_SendMsg(t *testing.T) {
+	type fields struct {
+		appKey   string
+		secret   string
+		basePath string
+	}
+	type args struct {
+		msg Msg
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    MsgResult
+		wantErr bool
+	}{
+		{
+			name: "发送消息",
+			args: args{msg: Msg{
+				From:             "",
+				Ope:              0,
+				To:               "",
+				Type:             0,
+				Body:             "",
+				Antispam:         "",
+				AntispamCustom:   "",
+				Option:           "",
+				Pushcontent:      "",
+				Payload:          "",
+				Ext:              "",
+				Forcepushlist:    "",
+				Forcepushcontent: "",
+				Forcepushall:     "",
+				Bid:              "",
+				UseYidun:         0,
+				MarkRead:         0,
+				CheckFriend:      false,
+			}},
+			want:    MsgResult{},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := NetEaseIM{
+				appKey:   tt.fields.appKey,
+				secret:   tt.fields.secret,
+				basePath: tt.fields.basePath,
+			}
+			got, err := n.SendMsg(tt.args.msg)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SendMsg() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SendMsg() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
