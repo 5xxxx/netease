fa5dfd93da6ae62ca5c416ef9dfd84a0b80f1288
gq-tang <372045127@qq.com>
2020-12-09 13:40:15 +0800

    增加发送广播消息

--- msg.go
+++ msg.go
@@ -359,3 +359,55 @@ func (n NetEaseIM) CleanNOS(nos Nos) (string, error) {
 
 	return resp.Data.Taskid, nil
 }
+
+/*
+参数	类型	必须	说明
+body		String	是	广播消息内容，最大4096字符
+from		String	否	发送者accid, 用户帐号，最大长度32字符，必须保证一个APP内唯一
+isOffline	String	否	是否存离线，true或false，默认false
+ttl			int		否	存离线状态下的有效期，单位小时，默认7天
+targetOs	String	否	目标客户端，默认所有客户端，jsonArray，格式：["ios","aos","pc","web","mac"]
+*/
+type BroadcastMsgReq struct {
+	Body      string   `json:"body"`
+	From      string   `json:"from"`
+	IsOffline bool     `json:"isOffline"`
+	TTL       int      `json:"ttl"`
+	TargetOS  []string `json:"targetOs"`
+}
+
+type BroadcastMsgResp struct {
+	Code int `json:"code"`
+	Msg  struct {
+		ExpireTime  int64    `json:"expireTime"`
+		Body        string   `json:"body"`
+		CreateTime  int64    `json:"createTime"`
+		IsOffline   bool     `json:"isOffline"`
+		BroadcastID int64    `json:"broadcastId"`
+		TargetOs    []string `json:"targetOs"`
+	} `json:"msg"`
+}
+
+/*
+接口描述
+1、使用广播消息前，请务必阅读注意事项，详见关于广播消息。
+2、广播消息，可以对应用内的所有用户发送广播消息，广播消息目前暂不支持第三方推送（APNS、小米、华为等）；
+3、广播消息支持离线存储，并可以自定义设置离线存储的有效期，最多保留最近100条离线广播消息；
+4、此接口受频率控制，一个应用一分钟最多调用10次，一天最多调用1000次，超过会返回416状态码；
+5、该功能目前需申请开通，详情可咨询您的客户经理。
+*/
+func (n NetEaseIM) SendBroadcastMsg(msg BroadcastMsgReq) (*BroadcastMsgResp, error) {
+	b, err := n.request(path.SendBroadcastMsg, structToMap(msg))
+	if err != nil {
+		return nil, err
+	}
+	var resp BroadcastMsgResp
+	if err = json.Unmarshal(b, &resp); err != nil {
+		return nil, err
+	}
+
+	if resp.Code != 200 {
+		return nil, errors.New(string(b))
+	}
+	return &resp, nil
+}
--- path/path.go
+++ path/path.go
@@ -49,6 +49,7 @@ const (
 	FileUpload         Path = "msg/fileUpload.action"
 	NOSClean           Path = "job/nos/del.action"
 	Recall             Path = "msg/recall.action"
+	SendBroadcastMsg   Path = "/msg/broadcastMsg.action"
 )
 
 const (
